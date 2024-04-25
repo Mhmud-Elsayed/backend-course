a0746e0fd49a6f41ce0ba3871b36368f5579450e
Mhmud-Elsayed <houdaaaelsayed22222@gmail.com>
2024-04-25 03:19:52 +0200

    set oop task

---
+++ oop_task/require/classB.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace classB;
+class B{
+
+public function __construct() {
+    echo "B done";
+    
+}
+
+
+}
---
+++ oop_task/require/index.php
@@ -0,0 +1,7 @@
+<?php
+
+require("classA.php");
+require("classB.php");
+
+$obj1= new classA\A();
+$obj= new classB\B();
